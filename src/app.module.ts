dd5f8d109da877745ede5f7f283fb6830df077c8
unknown <knameless27@gmail.com>
2023-04-18 14:04:55 -0500

    feat: avances en esquema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ItemsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(process.env.DB_URI),
+        ItemsModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
